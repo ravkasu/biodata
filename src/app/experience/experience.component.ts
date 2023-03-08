3d9d9c919a21f79c8fa706805c8b830d196b3073
ravkasu <ravkasu@gmail.com>
2023-03-08 19:54:05 +0530

    experience page update

--- src/app/experience/experience.component.ts
+++ src/app/experience/experience.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RestAPIService  } from '../services/rest-api.service';
 
 @Component({
   selector: 'app-experience',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./experience.component.css']
 })
 export class ExperienceComponent {
+  expData: any;
 
+  constructor(private http: HttpClient, private restAPIService : RestAPIService ) {}
+
+  ngOnInit(): void {
+    this.getProjectsData();
+  }
+  getProjectsData(){
+    this.restAPIService .getProjectsAPI().subscribe((data: any) => {
+      console.log(data);
+    });
+    this.http.get('assets/json/experienceData.json').subscribe((response: any) => {
+        this.expData = response;
+        console.log(response);
+      },
+      (error: any) => {
+        console.error(error);
+      }
+    );
+  }
 }
